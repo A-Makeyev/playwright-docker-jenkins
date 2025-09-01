7aa5669ccd547e06ad3190289282f0306157f043
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2025-09-01 23:55:22 +0300

    up

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,11 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 
 export default defineConfig({
     testDir: './tests',
-    outputDir: 'test-results/',
-    fullyParallel: true,
-    forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
     timeout: 30_000,
-    expect: {
-        timeout: 10_000
-    },
-
+    workers: '50%',
+    retries: 0,
     use: {
         baseURL: 'https://www.anzu.io',
         headless: !!process.env.CI,
@@ -21,46 +14,15 @@ export default defineConfig({
         video: 'retain-on-failure',
         screenshot: 'only-on-failure',
     },
-
     reporter: [
-        ['html'],
-        ['junit', { 
-        outputFile: 'junit-results/results.xml',
-        includeProjectInTestName: true 
-        }],
-        ['allure-playwright', {
-        detail: true,
-        outputFolder: 'allure-results',
-        suiteTitle: false,
-        environmentInfo: {
-            framework: 'Playwright',
-            node_version: process.version,
-        }
-        }],
-        ['list']
+        ['list'],
+        ['allure-playwright'],
+        ['junit', { outputFile: 'test-results/results.xml' }]
     ],
-    
     projects: [
-        {
-            name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
-        },
-        {
-            name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
-        },
-        {
-            name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
-        },
-        // {
-        //     name: 'Mobile Chrome',
-        //     use: { ...devices['Pixel 5'] },
-        // },
-        // {
-        //     name: 'Mobile Safari',
-        //     use: { ...devices['iPhone 12'] },
-        // },
+        { name: 'Chromium', use: { browserName: 'chromium' } },
+        { name: 'Firefox', use: { browserName: 'firefox' } },
+        { name: 'WebKit', use: { browserName: 'webkit' } },
     ],
 })
 
